8e2ca93dac54723c632e22a22202cb8e905ff1d1
Taiwo Oyewole <oyewoletaiwo94@gmail.com>
2020-09-17 02:27:07 +0100

    modify changes on files

--- src/script.js
+++ src/script.js
@@ -51,7 +51,64 @@ var chart = new Chart(ctl, {
     options: {}
 });
 
+// Line Chart for mobile view 
 
+var ctl = document.getElementById('mobileLineChart').getContext('2d');
+var chart = new Chart(ctl, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: [' 1/28/20 ', '2/3/20 ', '2/3/20 ', '2/17/20', ' 2/24/20'],
+        datasets: [{
+            label: 'Revenue',
+            backgroundColor: '#AFCDFD',
+            borderColor: '#73A8FC',
+            data: [0, 12, 5, 30, 0, 0]
+        }]
+    },
+
+    options: {}
+});
+
+var ctl = document.getElementById('mobileLineChart2').getContext('2d');
+var chart = new Chart(ctl, {
+    type: 'line',
+
+    data: {
+        labels: [' 1/28/20 ', '2/3/20 ', '2/3/20 ', '2/17/20', ' 2/24/20'],
+        datasets: [{
+            label: 'Amount',
+            backgroundColor: '#AFCDFD',
+            borderColor: '#73A8FC',
+            data: [0, 12, 5, 30, 0, 0]
+        }]
+    },
+
+    options: {}
+});
+
+var ctl = document.getElementById('mobileLineChart3').getContext('2d');
+var chart = new Chart(ctl, {
+    type: 'line',
+
+    data: {
+        labels: [' 1/28/20 ', '2/3/20 ', '2/3/20 ', '2/17/20', ' 2/24/20'],
+        datasets: [{
+            label: 'Amount',
+            backgroundColor: '#AFCDFD',
+            borderColor: '#73A8FC',
+            data: [0, 12, 5, 30, 0, 0]
+        }]
+    },
+
+    options: {}
+});
+
+
+
+// *************** Doughnut Chart ******************
 var ctx = document.getElementById('myChart').getContext('2d');
 
 var myDoughnutChart = new Chart(ctx, {
@@ -67,13 +124,7 @@ var myDoughnutChart = new Chart(ctx, {
                 '#56CCF2',
                 '#F2C94C',
                 '#9B51E0'
-            ],
-            
-            seriesDefaults: {
-                type: "donut",
-                startAngle: 90,
-                holeSize: 180,
-            }
+            ]
         }],
         
     },
@@ -168,5 +219,93 @@ var myDoughnutChart = new Chart(ctx, {
 
 });
 
+// Doughnut chart for mobile screen 
+var ctx = document.getElementById('myMobileChart').getContext('2d');
 
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
 
+    data: {
+
+        datasets: [{
+            data: [30, 20, 30, 20, 10],
+            backgroundColor: [
+                '#EB5757',
+                '#F2994A',
+                '#56CCF2',
+                '#F2C94C',
+                '#9B51E0'
+            ],
+        }],
+        
+    },
+
+   
+    options: {
+        responsive: true,
+        legend: {
+            display: false,
+        }
+    }
+
+});
+
+var ctx = document.getElementById('myMobileChart2').getContext('2d');
+
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+
+    data: {
+
+        datasets: [{
+            data: [30, 20, 30, 20, 10],
+            backgroundColor: [
+                '#EB5757',
+                '#F2994A',
+                '#56CCF2',
+                '#F2C94C',
+                '#9B51E0'
+            ],
+        }],
+        
+    },
+
+   
+    options: {
+        responsive: true,
+        legend: {
+            display: false,
+        }
+    }
+
+});
+
+var ctx = document.getElementById('myMobileChart3').getContext('2d');
+
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+
+    data: {
+
+        datasets: [{
+            data: [30, 20, 30, 20, 10],
+            backgroundColor: [
+                '#EB5757',
+                '#F2994A',
+                '#56CCF2',
+                '#F2C94C',
+                '#9B51E0'
+            ],
+        }],
+        
+    },
+
+   
+    options: {
+        responsive: true,
+        legend: {
+            display: false,
+        }
+    }
+
+});
